fef8a8565a2e91ec70f7540095286436abe153de
anushkabishnoi <anushkabishnoi9@gmail.com>
2024-12-01 17:43:52 +0530

    java and cpp compilers ok ✅
    Signed-off-by: anushkabishnoi <anushkabishnoi9@gmail.com>

--- backend/myapp/tests.py
+++ backend/myapp/tests.py
@@ -17,3 +17,38 @@ class CompileCodeTests(TestCase):
         response = self.client.post(url, json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertIn('Error:', response.json()['output'])
+
+    def test_valid_java_code(self):
+        url = reverse('compile_code')
+        data = {
+            'code':'public class Main { public static void main(String[] args) {System.out.println("Hello, Java!");}}', 'language': 'java'
+        }
+        response = self.client.post(url, json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Hello, Java!', response.json()['output'])
+        
+    def test_invalid_java_code(self):
+        url = reverse('compile_code')
+        data = {
+            'code': 'public class Main { public static void main(String[] args) { System.out.println("Hello, Java!"}}','language': 'java'
+        }
+        response = self.client.post(url, json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Error:', response.json()['output'])
+
+    def test_valid_cpp_code(self):
+        url = reverse('compile_code')
+        data = {
+            'code': '#include <iostream>\nusing namespace std;\nint main() { cout << "Hello, C++!" << endl; return 0; }', 'language': 'cpp'
+        }
+        response = self.client.post(url, json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Hello, C++!', response.json()['output'])
+
+    def test_invalid_cpp_code(self):
+        url = reverse('compile_code') 
+        data = { 'code': '#include <iostream>\nusing namespace std;\nint main() { cout << "Hello, C++!" << endl return 0; }', 'language': 'cpp' } 
+        response = self.client.post(url, json.dumps(data), content_type='application/json') 
+        self.assertEqual(response.status_code, 200) 
+        self.assertIn('Error:', response.json()['output'])
+        
--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -24,23 +24,27 @@ def compile_with_docker(code, language):
     # Determine the file extension and Docker image based on language
     extensions = {'python': 'py', 'java': 'java', 'cpp': 'cpp'}
     docker_images = {'python':'python:3.8-slim', 'java': 'openjdk:11-slim', 'cpp': 'gcc:latest'}
-    file_name = f"temp.{extensions[language]}"
+
+    if language == 'java':
+        file_name = "Main.java" # to ensure filename is MAin.java for java (causing problems)
+    else:
+        file_name = f"temp.{extensions[language]}"
     docker_image = docker_images[language]
 
     # using absolute path to ensure the file is created in the current working directory
-    file_path = os.path.join(os.getcwd(), file_name)
+    file_path = os.path.join(os.getcwd(), file_name)   # previously 'temp.py' 
 
     # write code to temp file
-    with open(file_path, 'w') as f:
+    with open(file_path, 'w') as f:  # 'temp.py' inplace of file_path here (previously)
         f.write(code)
 
     # command to run the docker container
     if language == 'python':
         cmd = ['docker', 'run', '--rm', '-v', f"{os.getcwd()}:/app", docker_image, 'python', f'/app/{file_name}']
     elif language == 'java':
-        cmd = ['docker', 'run', '--rm', '-v', f"{os.getcwd()}:/app", docker_image, 'sh', '-c', f"javac {file_name} && java {file_name.split('.')[0]}"]
+        cmd = ['docker', 'run', '--rm', '-v', f"{os.getcwd()}:/app", docker_image, 'sh', '-c', f"javac /app/{file_name} && java -cp /app {file_name.split('.')[0]}"]
     elif language == 'cpp':
-        cmd = ['docker', 'run', '--rm', '-v', f"{os.getcwd()}:/app", docker_image, 'sh', '-c', f"g++ {file_name} -o main && ./main"]
+        cmd = ['docker', 'run', '--rm', '-v', f"{os.getcwd()}:/app", docker_image, 'sh', '-c', f"g++ /app/{file_name} -o /app/main && /app/main"]
 
     # run the docker container to execute the python code
     try:
